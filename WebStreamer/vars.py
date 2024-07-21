01873163767305555a8fc95dd03b1c83b36b47db
TahirDea <tahirdea99@mynes.com>
2024-07-21 17:44:17 +0530

    Update vars.py

--- WebStreamer/vars.py
+++ WebStreamer/vars.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 
 class Var(object):
-    MULTI_CLIENT = False
+    MULTI_CLIENT = True
     API_ID = int(environ.get("API_ID"))
     API_HASH = str(environ.get("API_HASH"))
     BOT_TOKEN = str(environ.get("BOT_TOKEN"))
